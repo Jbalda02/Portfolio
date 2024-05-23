cc0985bfac9e035f4b123e9b3e07de9239fe0bff
joseb <jbaldac02@gmail.com>
2024-05-23 13:50:11 -0500

    Style changes to SmallGallery To do proyects section

--- portfolio-final/src/App.tsx
+++ portfolio-final/src/App.tsx
@@ -10,7 +10,8 @@ import data from './jsons/Technologies.json' assert { type: 'json' };
 
 //Images Imports
 import section1image from "./assets/Images//Asap_React_140120.jpg";
-import section2image from "./assets/Images//HobbiesBanner.jpeg";
+import section2image from "./assets/Images//Asap_React_140120.jpg";
+
 //One Small Gallery is for Technologies Icon that i domain
 //And the other Small Gallery is for small Proyects
 interface Technology {
@@ -41,14 +42,20 @@ function App() {
     <div className="flex-col">
       <NavigationBar />
       <Hero></Hero>
+      <div className="bg-purple-600">
       <PreSection ImageToLoad={section1image}></PreSection>
-        <div className="flex flex-row m-4 justify-around   gap-12 flex-wrap">
+        </div>
+        <div className="flex flex-row  justify-around m-b-3  gap-12 flex-wrap bg-gradient-to-b from-purple-600 via-purple-700 to-blue-800 pb-12">
          <SmallGallery technologies={FrontEndTechnologies} title={titleSGFront}></SmallGallery>
          <SmallGallery technologies={BackEndTechnologies} title={titleSGBack}></SmallGallery>
          <SmallGallery technologies={ManagmentTechnologies} title={titleSG}></SmallGallery>
         </div>
+        <div className="bg-blue-800">
       <BigGallery></BigGallery>
+      </div>
+      <div className="bg-blue-800">
       <PreSection ImageToLoad={section2image}></PreSection>
+     </div>
       <TextSection></TextSection>
       <TextSection></TextSection>
       <Footer></Footer>
